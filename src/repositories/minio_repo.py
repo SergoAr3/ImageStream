9b64caad3966bcdac965f5f75564a5aa5fcaaeb1
Серёжа <105739352+SergoAr3@users.noreply.github.com>
2024-07-29 16:25:09 +0300

    Update minio_repo.py

--- src/repositories/minio_repo.py
+++ src/repositories/minio_repo.py
@@ -28,26 +28,3 @@ class MinioRepository(IMinioRepository):
             return [obj for obj in objs]
         except Exception as e:
             logger.error(e)
-
-# m = MinioRepository(c, conf.MINIO_BUCKET_NAME)
-# r = RedisRepository(Redis(host=conf.REDIS_HOST, port=conf.REDIS_PORT))
-#
-#
-# async def main():
-#     obj = await m.get_all_objects()
-#     obj_data = {
-#         'name': obj[0].object_name,
-#         'size': f'{obj[0].size * 0.001:.2f} KB',
-#     }
-#
-#     obj = json.dumps(obj_data)
-#     await r.push(obj)
-#     o = await r.pop()
-#     await r.close()
-#     image = json.loads(o)
-#
-#     return type(image)
-#
-#
-# if __name__ == "__main__":
-#     print(asyncio.run(main()))
